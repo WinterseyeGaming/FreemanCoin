88254c3e03fc76516b4d5807200c75253665e7a3
WinterseyeGaming <xscript@me.com>
2015-07-06 13:15:49 +0100

    changes

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SMC");
-    case mBTC: return QString("mSMC");
-    case uBTC: return QString::fromUtf8("μSMC");
+    case BTC: return QString("FMC");
+    case mBTC: return QString("mFMC");
+    case uBTC: return QString::fromUtf8("μFMC");
     default: return QString("???");
     }
 }
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SmallChange");
-    case mBTC: return QString("Milli-SmallChange (1 / 1,000)");
-    case uBTC: return QString("Micro-SmallChange (1 / 1,000,000)");
+    case BTC: return QString("FreedomCoin");
+    case mBTC: return QString("Milli-FreedomCoin (1 / 1,000)");
+    case uBTC: return QString("Micro-FreedomCoin (1 / 1,000,000)");
     default: return QString("???");
     }
 }
