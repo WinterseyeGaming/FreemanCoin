82da0682d4c5614f458d6f1054be0f277a9cfa04
WinterseyeGaming <xscript@me.com>
2015-07-06 14:34:05 +0100

    changes

--- src/main.cpp
+++ src/main.cpp
@@ -2038,17 +2038,17 @@ bool LoadBlockIndex(bool fAllowNew)
         if (fTestNet)
         {
             block.nTime    = 1436184618;
-            block.nNonce   = 386402991;
+            block.nNonce   = 387295665;
         }
 
         //// debug print
         printf("%s\n", block.GetHash().ToString().c_str());
         printf("%s\n", hashGenesisBlock.ToString().c_str());
         printf("%s\n", block.hashMerkleRoot.ToString().c_str());
-        assert(block.hashMerkleRoot == uint256("0x5a2e19825b4162f68602039040f1e05d9f924ff00a3aff7327ca6abd6f3279bc"));
+        assert(block.hashMerkleRoot == uint256("0xe94cd5de59cbd07bd15e9dfe58a4bbef5262b705eb2a8a80199289892879df9a"));
 
         // If genesis block hash does not match, then generate new genesis hash.
-        if (false && block.GetHash() != hashGenesisBlock)
+        if (true && block.GetHash() != hashGenesisBlock)
         {
             printf("Searching for genesis block...\n");
             // This will figure out a valid hash and Nonce if you're
---
+++ src/obj/build.h
@@ -0,0 +1,2 @@
+// No build information available
+#define BUILD_DATE "2015-07-06 13:15:49 +0100"
